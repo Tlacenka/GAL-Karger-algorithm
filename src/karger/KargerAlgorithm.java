8683deb9c3f6570fb12167358f111fb69ecd1313
Katka Pilatova <tlacencin@gmail.com>
2017-11-02 09:21:20 +0100

    code structure changes
    - images moved to the src/karger folder
    - .form files, manifest, nbproject folder deleted
    - .gitignore introduced
    - in the code, automatically generated comments removed
    - file titles, initial comments improved
    - build file fixed
    - startApp replaced by KargerAlgorithm.java
    - all files from gui moved to src (the whole app is the gui anyway)

--- src/karger/KargerAlgorithm.java
+++ src/karger/KargerAlgorithm.java
@@ -1,21 +1,24 @@
 /**
  * This file contains the main class for Karger's Algorithm
- * @author Katka Pilatova (xpilat05), Michal Tabasek
+ * @author Katerina Pilatova (xpilat05), Michal Tabasek (xtabas02)
  * @date 2017
  */
 
+
 package karger;
 
-import gui.mainWindow;
 
 public class KargerAlgorithm {
 
    public static void main(String[] args) {
-      System.out.println("Hello World");
-      
+
+      // Run the application
+      (new mainWin()).show();
+
+      /* This was in the code before (in startApp)
+       * mainWin mainWindow = new mainWin();
+       * mainWindow.setVisible(true); 
+       */
         
    }
-   
-   
-
 }
