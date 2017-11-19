ac1d910fbca86b672f4a2bfd0f40f32588618657
Michal <tabasekm@gmail.com>
2017-11-19 23:20:40 +0100

    update open/save file + code required for the designer
    there was a confusion here and I'm so sorry for that, I'm still learning :D

--- src/karger/KargerAlgorithm.java
+++ src/karger/KargerAlgorithm.java
@@ -1,19 +1,27 @@
 /**
  * This file contains the main class for Karger's Algorithm
- * @author Katka Pilatova (xpilat05), Michal Tabasek
+ * @author Katerina Pilatova (xpilat05), Michal Tabasek (xtabas02)
  * @date 2017
  */
 
+
 package karger;
 
+/*<<<<<<< HEAD
+=======
+
+>>>>>>> 5270d0218e6b327c8c84cfcc06cf638ac10e2eec*/
 public class KargerAlgorithm {
 
    public static void main(String[] args) {
-      System.out.println("Hello World");
-      
+
+      // Run the application
+      (new MainWin()).show();
+
+      /* This was in the code before (in startApp)
+       * mainWin mainWindow = new mainWin();
+       * mainWindow.setVisible(true); 
+       */
         
    }
-   
-   
-
 }
